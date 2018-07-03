fd36e493030b5bd46b0cdd2a3fe7db4d2f5ff614
Paul Casto <paul.c.casto@gmail.com>
2018-07-03 12:41:17 -0400

    better browser-browser messaging (for pause/play); whitespace cleanup; favicon.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const C = require('constants');
-const net = require('net');
-
 var express = require('express');
 var app = require('express')();
 var server = require('http').createServer(app);
@@ -12,6 +9,7 @@ var listenPort = process.env.PATIOBAR_PORT || 3000;
 
 var pianobarStart = process.env.PIANOBAR_START || process.env.HOME + '/Patiobar/patiobar.sh start';
 var pianobarStop  = process.env.PIANOBAR_STOP  || process.env.HOME + '/Patiobar/patiobar.sh stop-pianobar';
+var pianobarPlaying = false;
 
 app.use(express.static('views/images'));
 var pianobarOffImageURL = '../On_Off.png';
@@ -26,75 +24,84 @@ function readCurrentSong() {
 
 	if (currentSong) {
 	   var a = currentSong.split(',,,');
-	   console.log(a);
-           if (a[0] == "PIANOBAR_STOPPED") {
-              ProcessCTL('stop');
-           } else {
-	      io.emit('start', { artist: a[0], title: a[1], album: a[2], coverArt: a[3], rating: a[4], stationName: a[5] });
-           }
+	   // console.log(a);
+	   if (a[0] == "PIANOBAR_STOPPED") {
+		 ProcessCTL('stop');
+	   } else {
+		 io.emit('start', { artist: a[0], title: a[1], album: a[2], coverArt: a[3], rating: a[4], stationName: a[5], isplaying: pianobarPlaying });
+	   }
 	}
-
 }
 
 function ProcessCTL(action) {
-        switch(action) {
-          case 'start':
-	    console.log('Starting Pianobar');
-            try {
-              require('child_process').execSync("cat", [fifo]); //clear old buffer
-              console.log('Buffer should be cleared');
-              require('child_process').execSync(pianobarStart);
-              console.log('Pianobar process should have started');
-            }
-            catch (err) {
-              console.log('Error in starting Pianobar: ' + err.message);
-              return;
-            }
-            break;
+	switch(action) {
+	  case 'start':
+		console.log('Starting Pianobar');
+		try {
+		  require('child_process').execSync("cat", [fifo]); //clear old buffer
+		  console.log('Buffer should be cleared');
+		  require('child_process').execSync(pianobarStart);
+		  console.log('Pianobar process should have started');
+		  pianobarPlaying = true;
+		}
+		catch (err) {
+		  console.log('Error in starting Pianobar: ' + err.message);
+		  return;
+		}
+		break;
 
-          case 'stop':
-	    console.log('Stopping Pianobar');
-	      PidoraCTL('q');
-	      io.emit('stop', { artist: '', title: '', album: '', coverArt: pianobarOffImageURL, rating: '', stationName: ''  });
-              fs.writeFile(process.env.HOME + '/.config/pianobar/currentSong', 'PIANOBAR_STOPPED,,,,', function (err) {
-                if (err) return console.log(err);
-                console.log('Stop entry made in currentSong file!');
-              });
-           break;
+	  case 'stop':
+		console.log('Stopping Pianobar');
+		try {
+		  require('child_process').execSync("cat", [fifo]); //clear old buffer
+			  console.log('Buffer should be cleared');
+		  PidoraCTL('q');
+		  pianobarPlaying = false;
+		  io.emit('stop', { artist: '', title: '', album: '', coverArt: pianobarOffImageURL, rating: '', stationName: '', isplaying: false	});
+		  fs.writeFile(process.env.HOME + '/.config/pianobar/currentSong', 'PIANOBAR_STOPPED,,,,', function (err) {
+			if (err) return console.log(err);
+			console.log('Stop entry made in currentSong file!');
+		  });
+		}
+		catch (err) {
+		  console.log('Error in stopping Pianobar: ' + err.message);
+		  return;
+		}
+		break;
 
-          default:
-            console.log('Unrecognized process action: ' + action);
-            break;
-         }
+	  default:
+		console.log('Unrecognized process action: ' + action);
+		break;
+	}
 }
 
 
 function PidoraCTL(action) {
 	fs.open(fifo, 'w', 0644, function(error, fd) {
 	  if (error) {
-	    if (fd) {
-	      fs.close(fd);
-	    }
-	    console.log('Error opening fifo: ' + error);
-	    return;
+		if (fd) {
+		  fs.close(fd);
+		}
+		console.log('Error opening fifo: ' + error);
+		return;
 	  }
 
 	  buf = new Buffer.from(action);
 	  fs.write(fd, buf, 0, action.length, null, function(error, written, buffer) {
-	    if (fd) {
-	      fs.close(fd, function(err) {
-	        if (err) console.log('Error closing fifo: ' + error);
-              });
-	    }
-	    if (error) {
-	      console.log('Error writing to fifo: ' + error);
-	    } else {
-	      if (written == action.length) {
-	        console.log(action.trim() + ' has been written successfully!');
-	      } else {
-	        console.log('Error: Only wrote ' + written + ' out of ' + action.length + ' bytes to fifo.');
-	      }
-	    }
+		if (fd) {
+		  fs.close(fd, function(err) {
+			if (err) console.log('Error closing fifo: ' + error);
+			  });
+		}
+		if (error) {
+		  console.log('Error writing to fifo: ' + error);
+		} else {
+		  if (written == action.length) {
+			console.log(action.trim('\n') + ' has been written successfully!');
+		  } else {
+			console.log('Error: Only wrote ' + written + ' out of ' + action.length + ' bytes to fifo.');
+		  }
+		}
 	  });
 	});
 }
@@ -105,19 +112,24 @@ function readStations() {
 	io.emit('stations', { stations: stations });
 }
 
-
-io.on('disconnect', function(socket){
-	if (socket) {
-	  console.log('a user disconnected from:' + socket.request.connection.remoteAddress);
-	} else {
-	  console.log('a user disconnected');
-	}
-  });
+//function readPlayingStatus() {
+//	io.emit('playing', { status: pianoBarPlaying });
+//}
 
 io.on('connection', function(socket) {
 	console.log('a user connected from: ' + socket.request.connection.remoteAddress);
+	//readPlayingStatus();
 	readCurrentSong();
 	readStations();
+
+	socket.on('disconnect', function(){
+		try {
+		  console.log('a user disconnected from:' + socket.request.connection.remoteAddress);
+		}
+		catch (err) {
+		  console.log('a user disconnected');
+		}
+	});
 
 	socket.on('process', function (data) {
 		var action = data.action
@@ -125,7 +137,9 @@ io.on('connection', function(socket) {
 	});
 
 	socket.on('action', function (data) {
-		var action = data.action.substring(0, 1)
+		var action = data.action.substring(0, 1);
+		// rebroadcast changes - like 'pause' - but avoid circular
+		socket.broadcast.emit('action', { action: action});
 		PidoraCTL(action);
 	});
 
@@ -135,26 +149,24 @@ io.on('connection', function(socket) {
 		PidoraCTL(cmd);
 	});
 
-app.post('/start', function(request, response){
-	artist = request.query.artist;
-	title = request.query.title;
-	album = request.query.album;
-	coverArt = request.query.coverArt;
-	rating = request.query.rating;
-	stationName = request.query.stationName;
-	io.emit('start', { artist: artist, title: title, coverArt: coverArt, album: album, rating: rating, stationName: stationName });
-	readStations();
-	response.send(request.query);
+	app.post('/start', function(request, response){
+		artist = request.query.artist;
+		title = request.query.title;
+		album = request.query.album;
+		coverArt = request.query.coverArt;
+		rating = request.query.rating;
+		stationName = request.query.stationName;
+		readStations();
+		io.emit('start', { artist: artist, title: title, coverArt: coverArt, album: album, rating: rating, stationName: stationName, isplaying: pianobarPlaying });
+		response.send(request.query);
 
-});
+	});
 
-app.post('/lovehate', function(request, response) {
-	rating = request.query.rating;
+	app.post('/lovehate', function(request, response) {
+		rating = request.query.rating;
 
-	io.emit('lovehate', { rating: rating });
-
-	console.log(request.query);
-});
+		io.emit('lovehate', { rating: rating });
+	});
 
 
 });
--- views/js/main.js
+++ views/js/main.js
@@ -3,31 +3,31 @@ var app = angular.module('patiobarApp', []);
 app.factory('socket', function ($rootScope) {
   var socket = io.connect();
   return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      })
-    }
+	on: function (eventName, callback) {
+	  socket.on(eventName, function () {
+		var args = arguments;
+		$rootScope.$apply(function () {
+		  callback.apply(socket, args);
+		});
+	  });
+	},
+	emit: function (eventName, data, callback) {
+	  socket.emit(eventName, data, function () {
+		var args = arguments;
+		$rootScope.$apply(function () {
+		  if (callback) {
+			callback.apply(socket, args);
+		  }
+		});
+	  })
+	}
   };
 });
 
 function ProcessController($scope, socket) {
-        $scope.process = function(action) {
-             socket.emit('process', { action: action });
-        }
+		$scope.process = function(action) {
+			 socket.emit('process', { action: action });
+		}
 	$("#stop").click(function() {
 		$(this).children().toggleClass('glyphicon-pause glyphicon-play');
 	});
@@ -47,59 +47,108 @@ function StationController($scope, socket) {
 		}
 
 		$scope.stations = s;
-                document.getElementById("stations").className = "";
+		document.getElementById("stations").className = "";
 	});
 
-// factory created two 'start' handlers - perhaps better to combine them
-//	socket.on('start', function(msg) {
-//                var stationName = msg.stationName.substr(0, msg.stationName.length - 6);
-//		$scope.stationName = stationName;
-//        });
+	socket.on('start', function(msg) {
+		// in case msg arrives without stationName set
+		try {
+			var stationName = msg.stationName.substr(0, msg.stationName.length - 6);
+			$scope.stationName = stationName;
+		}
+		catch (err) {
+		}
+	});
 
 	$scope.changeStation = function(stationId) {
 		socket.emit('changeStation', { stationId: stationId });
 	}
-        document.getElementById("controls").className = "";
+	document.getElementById("controls").className = "";
 }
 
 function SongController($scope, socket) {
 	socket.on('start', function(msg) {
+		document.getElementById("controls").className = "";
 		var aa = 'on ' + msg.album + ' by ' + msg.artist;
 		$scope.albumartist = aa;
 		$scope.src = msg.coverArt;
 		$scope.alt = msg.album;
 		$scope.title = msg.title;
 		$scope.rating = msg.rating;
+		$scope.playing = msg.isplaying;
+		alert($scope.playing);
+		$scope.setPausePlayDisplay();
 
 		if (msg.rating == 1) {
 			document.getElementById("love").className = "btn btn-success pull-left";
 		} else {
 			document.getElementById("love").className = "btn btn-default pull-left";
-
 		}
-// combinining into one start handler
-var stationName = msg.stationName.substr(0, msg.stationName.length - 6);
-$scope.stationName = stationName;
 	});
 
-        socket.on('stop', function(msg) {
-                var aa = 'pianobar turned off. click the play key to start';
-                $scope.albumartist = aa;
-                $scope.src = msg.coverArt;
-                $scope.alt = 'pianobar off';
-                $scope.title = msg.title;
-                $scope.rating = msg.rating;
-                document.getElementById("controls").className = "hidden-controls";
-                document.getElementById("stations").className = "hidden-controls";
-        });
+	socket.on('stop', function(msg) {
+		$scope.playing = false;
+		var aa = 'pianobar turned off. click the play key to start';
+		$scope.albumartist = aa;
+		$scope.src = msg.coverArt;
+		$scope.alt = 'pianobar off';
+		$scope.title = msg.title;
+		$scope.rating = msg.rating;
+		document.getElementById("controls").className = "hidden-controls";
+		//document.getElementById("stations").className = "hidden-controls";
+	});
+
+	socket.on('action', function(msg) {
+		var action = msg.action;
+		switch(action) {
+		case 'P':
+			alert('P - should start now');
+			$scope.playing = true;
+			$scope.setPausePlayDisplay();
+			break;
+		case 'S':
+			alert('S - should stop now');
+			$scope.playing = false;
+			$scope.setPausePlayDisplay();
+			break;
+		default:
+			//alert('unknown action: ' + action);
+			// shouldn't care about other messages, but if we do, add handlers
+			break;
+		}
+
+		//$(this).children().toggleClass('glyphicon-pause glyphicon-play');
+		//$("#pauseplay").click();
+	});
 
 	$scope.sendCommand = function(action) {
 		socket.emit('action', { action: action });
 	}
 
-	$("#pauseplay").click(function() {
-		$(this).children().toggleClass('glyphicon-pause glyphicon-play');
-	});
+//	$scope.togglePausePlay = function(pauseplay) {
+//	  $scope.playing = false;
+//	  $scope.pauseplayButton = $scope.subscribed ? 'Unsubscribe' : 'Subscribe';
+	$scope.togglePausePlay= function() {
+		$scope.playing ? $scope.sendCommand('S') : $scope.sendCommand('P');
+		$scope.playing = !$scope.playing;
+		$scope.setPausePlayDisplay()
+	};
+
+	$scope.setPausePlayDisplay= function() {
+		if ($scope.playing) {
+			document.getElementById('pauseplayICON').className = 'glyphicon glyphicon-pause';
+		} else {
+			//document.querySelector('#pauseplay span').className = 'glyphicon glyphicon-play';
+			document.getElementById('pauseplayICON').className = 'glyphicon glyphicon-play';
+		}
+	};
+
+
+
+//	$("#pauseplay").click(function() {
+//		$(this).children().toggleClass('glyphicon-pause glyphicon-play');
+		//$(this).children().ng-click="sendCommand('P');
+//	});
 
 	socket.on('lovehate', function(msg) {
 		if (msg.rating == 1) {
